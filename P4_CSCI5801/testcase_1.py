2877570a7619d43ff23d829c9d6f34370f4a57d9
brenn682 <brenn682@umn.edu>
2022-11-28 22:58:07 -0600

    test_output sucks less

--- P4_CSCI5801/testcase_1.py
+++ P4_CSCI5801/testcase_1.py
@@ -68,28 +68,30 @@ class Test_Remove_Lines(unittest.TestCase):
         back.ui.attr_nameQuery = 'test3a'
         back.name = 'helloworld.js'
         back.ui.filenameQuery = './testcase_files/helloworld.js'
-
-        self.assertEqual(back.remove_lines(), True, "Should be true")
+        print("Test_Remove_Lines (test_good) errors: ")
+        print(self.assertEqual(back.remove_lines(), True, "Should be true"))
     
     def test_file_not_found(self):
         # set conditions here
         back.ui.attr_nameQuery = 'test3b'
         back.ui.filenameQuery = './testcase_files/file_DNE.py'
-        self.assertEqual(back.remove_lines(), False, "Should be False")
+        print("Test_Remove_Lines (test_file_not_found) errors: ")
+        print(self.assertEqual(back.remove_lines(), False, "Should be False"))
 
 class Test_Select_Lines(unittest.TestCase):
     def test_good(self):
         # set conditions here
         back.ui.attr_nameQuery = 'test4a'
         back.ui.filenameQuery = './testcase_files/exampleocaml.ml.ml'
-
-        self.assertEqual(back.select_lines(), True, "Should be true")
+        print("Test_Select_Lines (test_good) errors: ")
+        print(self.assertEqual(back.select_lines(), True, "Should be true"))
     
     def test_file_not_found(self):
         # set conditions here
         back.ui.attr_nameQuery = 'test4b'
         back.ui.filenameQuery = './testcase_files/file_DNE.py'
-        self.assertEqual(back.select_lines(), False, "Should be False")
+        print("Test_Select_Lines (test_file_not_found) errors: ")
+        print(self.assertEqual(back.select_lines(), False, "Should be False"))
 
 class Test_Make_Tuple(unittest.TestCase):
     back.ui.attr_nameQuery = 'test5'
@@ -97,27 +99,33 @@ class Test_Make_Tuple(unittest.TestCase):
     
     def test_good(self):
         # set conditions here
-        self.assertEqual(back.make_tuple(2, 3, 32), True, "Should be true")
+        print("Test_Make_Tuple (test_good) errors: ")
+        print(self.assertEqual(back.make_tuple(2, 3, 32), True, "Should be true"))
     
     def test_empty_field(self):
         # set conditions here
-        self.assertEqual(back.make_tuple('', 14, 32), False, "Should be False")
+        print("Test_Make_Tuple (test_empty_field) errors: ")
+        print(self.assertEqual(back.make_tuple('', 14, 32), False, "Should be False"))
 
     def test_snd_ld_fst(self):
         # set conditions here
-        self.assertEqual(back.make_tuple(18, 17, 32), False, "Should be False")
+        print("Test_Make_Tuple (test_snd_ld_fst) errors: ")
+        print(self.assertEqual(back.make_tuple(18, 17, 32), False, "Should be False"))
 
     def test_fst_oob(self):
         # set conditions here
-        self.assertEqual(back.make_tuple(-1, 0, 32), False, "Should be False")
+        print("Test_Make_Tuple (test_fst_oob) errors: ")
+        print(self.assertEqual(back.make_tuple(-1, 0, 32), False, "Should be False"))
 
     def test_snd_oob(self):
         # set conditions here
-        self.assertEqual(back.make_tuple(32, 33, 32), False, "Should be False")
+        print("Test_Make_Tuple (test_snd_oob) errors: ")
+        print(self.assertEqual(back.make_tuple(32, 33, 32), False, "Should be False"))
 
     def test_adj_error(self):
         # set conditions here
-        self.assertEqual(back.make_tuple(5, 7, 32), False, "Should be False")
+        print("Test_Make_Tuple (test_adj_error) errors: ")
+        print(self.assertEqual(back.make_tuple(5, 7, 32), False, "Should be False"))
 
 class Test_Tuple_Lines(unittest.TestCase):
     def test_good(self):
@@ -125,21 +133,24 @@ class Test_Tuple_Lines(unittest.TestCase):
         back.ui.attr_nameQuery = 'test6a'
         back.ui.filenameQuery = './testcase_files/examplewcss.html'
         back.name = 'examplewcss.html'
-        self.assertEqual(back.tuple_lines(), True, "Should be true")
+        print("Test_Tuple_Lines (test_good) errors: ")
+        print(self.assertEqual(back.tuple_lines(), True, "Should be true"))
     
     def test_file_not_found_source(self):
         # set conditions here
         back.ui.attr_nameQuery = 'test6c'
         back.name = 'file_DNE.py'
         back.ui.filenameQuery = './testcase_files/file_DNE.py'
-        self.assertEqual(back.tuple_lines(), False, "Should be False")
+        print("Test_Tuple_Lines (test_file_not_found_source) errors: ")
+        print(self.assertEqual(back.tuple_lines(), False, "Should be False"))
 
     def test_file_not_found_solution(self):
         # set conditions here
         back.ui.attr_nameQuery = 'test6d'
         back.name = 'file_DNE.py'
         back.ui.filenameQuery = './solution_code/file_DNE/file_DNE.py'
-        self.assertEqual(back.tuple_lines(), False, "Should be False")
+        print("Test_Tuple_Lines (test_file_not_found_solution) errors: ")
+        print(self.assertEqual(back.tuple_lines(), False, "Should be False"))
 
 class Test_Choice_Made(unittest.TestCase):
     def test_LMS_good(self):
@@ -147,7 +158,8 @@ class Test_Choice_Made(unittest.TestCase):
         back.ui.attr_nameQuery = 'test7a'
         back.ui.filenameQuery = './testcase_files/primeNum.cc'
         back.LMS_choice = 'canvas'
-        self.assertEqual(back.choice_made('LMS', []), True, "Should be true")
+        print("Test_Choice_Made (test_good) errors: ")
+        print(self.assertEqual(back.choice_made('LMS', []), True, "Should be true"))
 
     def test_qType_good(self):
         # set conditions here
@@ -156,14 +168,16 @@ class Test_Choice_Made(unittest.TestCase):
         back.LMS_choice = 'canvas'
         back.qType_choice = 'multiple choice'
         sample_options = ['multiple choice','reordering','fill-in-the-blank','find the bug','indentation']
-        self.assertEqual(back.choice_made('qType', sample_options), True, "Should be true")
+        print("Test_Choice_Made (test_qType_good) errors: ")
+        print(self.assertEqual(back.choice_made('qType', sample_options), True, "Should be true"))
     
     def test_LMS_choice_invalid(self):
         # set conditions here
         back.ui.attr_nameQuery = 'test7c'
         back.ui.filenameQuery = './testcase_files/primeNum.cc'
         back.LMS_choice = 'wrong_option' # erroneous
-        self.assertEqual(back.choice_made('LMS', []), False, "Should be False")
+        print("Test_Choice_Made (test_LMS_choice_invalid) errors: ")
+        print(self.assertEqual(back.choice_made('LMS', []), False, "Should be False"))
 
     def test_qType_choice_invalid(self):
         # set conditions here
@@ -172,7 +186,8 @@ class Test_Choice_Made(unittest.TestCase):
         sample_options = ['multiple choice','reordering','fill-in-the-blank','find the bug','indentation']
         back.LMS_choice = 'moodle'
         back.qType_choice = 'invalid_option' # erroneous
-        self.assertEqual(back.choice_made('qType', sample_options), False, "Should be False")
+        print("Test_Choice_Made (test_qType_choice_invalid) errors: ")
+        print(self.assertEqual(back.choice_made('qType', sample_options), False, "Should be False"))
 
     def test_mode_parameter_invalid(self):
         # set conditions here
@@ -182,7 +197,8 @@ class Test_Choice_Made(unittest.TestCase):
         back.LMS_choice = 'moodle'
         back.qType_choice = 'reordering'
         # 'mode3' is erroneous
-        self.assertEqual(back.choice_made('mode3', sample_options), False, "Should be False")
+        print("Test_Choice_Made (test_mode_parameter_invalid) errors: ")
+        print(self.assertEqual(back.choice_made('mode3', sample_options), False, "Should be False"))
 
 class Test_QType_Select(unittest.TestCase):
     def test_good(self):
@@ -190,14 +206,16 @@ class Test_QType_Select(unittest.TestCase):
         back.ui.attr_nameQuery = 'test8a'
         back.ui.filenameQuery = './testcase_files/primeNum.cc'
         back.LMS_choice = 'blackboard'
-        self.assertEqual(back.qType_select(), True, "Should be true")
+        print("Test_QType (test_good) errors: ")
+        print(self.assertEqual(back.qType_select(), True, "Should be true"))
     
     def test_LMS_invalid(self):
         # set conditions here
         back.ui.attr_nameQuery = 'test8b'
         back.ui.filenameQuery = './testcase_files/primeNum.cc'
         back.LMS_choice = 'oops'
-        self.assertEqual(back.qType_select(), False, "Should be False")
+        print("Test_QType (test_LMS_invalid) errors: ")
+        print(self.assertEqual(back.qType_select(), False, "Should be False"))
 
 class Test_Create_Config_File(unittest.TestCase):
     
@@ -209,8 +227,8 @@ class Test_Create_Config_File(unittest.TestCase):
         back.LMS_choice = 'canvas'
         back.ui.attr_nameQuery = 'test9a'
         back.ui.filenameQuery = './testcase_files/primeNum.cc'
-
-        self.assertEqual(back.create_config_file(), True, "Should be true")
+        print("Test_Create_Config_File (test_good) errors: ")
+        print(self.assertEqual(back.create_config_file(), True, "Should be true"))
     
     def test_file_not_found(self):
         # set conditions here
@@ -220,7 +238,8 @@ class Test_Create_Config_File(unittest.TestCase):
         back.LMS_choice = 'canvas'
         back.ui.attr_nameQuery = 'test9b'
         back.ui.filenameQuery = './testcase_files/file_DNE.py'
-        self.assertEqual(back.create_config_file(), False, "Should be False")
+        print("Test_Create_Config_File (test_file_not_found) errors: ")
+        print(self.assertEqual(back.create_config_file(), False, "Should be False"))
 
 if __name__ == '__main__':
     # begin the unittest.main()
@@ -247,6 +266,8 @@ if __name__ == '__main__':
 
     tee = Tee(sys.stdout, open("test_output.txt", 'w'))
     sys.stdout = tee
+
+    print("Unit Test Results: \n\n")
 
     unittest.main()
 
